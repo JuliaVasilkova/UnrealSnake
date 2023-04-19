f60eb989b9b298e05c9f9b4e72f09e59c5b42add
Julia Vasilkova <julietta.vasilkova@gmail.com>
2023-04-19 14:16:26 +0300

    Snake tail overlap with head added

--- Source/UnrealSnake/TailElement.h
+++ Source/UnrealSnake/TailElement.h
@@ -24,4 +24,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// declare overlap begin function
+	UFUNCTION()
+		void OnTailOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 };
